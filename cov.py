09ce759ac3b954e8b74d3f5e6f451aacd9dcd15c
Lucas Batista <lucaophp@hotmail.com>
2022-09-22 22:02:19 -0300

    Create cov.py

---
+++ cov.py
@@ -0,0 +1,17 @@
+'''Apenas teste'''
+import numpy as np
+from scipy.stats import pearsonr
+import matplotlib.pyplot as plt
+
+np.random.seed(1)
+data1 = 20 * np.random.randn(1000) +10
+data2 = data1 + (10 * np.random.randn(1000) + 50)
+
+plt.scatter(data1, data2)
+plt.show()
+
+covariancia = np.cov(data1, data2)
+print(covariancia)
+
+corr, p = pearsonr(data1, data2)
+print('Correlação Pearson: {:.2f} - p-value: {}'.format(corr, p))
